a2236b7a970cc5b4920d4d24c41c755577feddb2
RostislawD <r.dolinskyi@lampalampa.net>
2024-12-19 13:17:01 +0200

    finished program

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-places = """66845   37619
-94793   99076
-76946   36179
-27374   48777
-47847   92154
-83270   97857
-19354   94331
-69716   58559"""
+filename = 'input.txt'
+with open(filename, 'r') as file:
+    places = file.readlines()
 
 column_1 = []
 column_2 = []
 
-places = places.splitlines()
-
 for place in places:
     part = place.split()
-    column_1.append(part[0])
-    column_2.append(part[1])
+    column_1.append(int(part[0]))
+    column_2.append(int(part[1]))
 
-column_1_str = " ".join(column_1)
-column_2_str = " ".join(column_2)
+column_1.sort()
+column_2.sort()
+
 suma = 0
+
+for c1, c2 in zip(column_1, column_2):
+    suma += abs(c1 - c2)
+
+print(suma)
