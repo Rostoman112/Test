78476bf1301324108bc8e7a0900d0afee3313746
RostislawD <r.dolinskyi@lampalampa.net>
2024-12-20 17:26:19 +0200

    finished task 1 from day one

--- Historian Hystoria/day_2_1.py
+++ Historian Hystoria/day_2_1.py
@@ -1,7 +1,24 @@
-filename = 'input_day_2.txt'
+filename = "input_day_2.txt"
 with open(filename, "r") as file:
     reports = file.readlines()
 
+reports = [list(map(int, line.split())) for line in reports]
+
+safe_count = 0
+
 for report in reports:
-    for number in report:
-        
+    is_safe = True
+    for i in range(len(report) - 1):
+        diff = abs(report[i] - report[i + 1])
+        if diff < 1 or diff > 3:
+            is_safe = False
+            break
+
+    if is_safe:
+        increasing = all(report[i] < report[i + 1] for i in range(len(report) - 1))
+        decreasing = all(report[i] > report[i + 1] for i in range(len(report) - 1))
+
+        if increasing or decreasing:
+            safe_count += 1
+
+print(safe_count)
